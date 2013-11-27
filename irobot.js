34cd0a9024d3a849c0034c27df6a0768a11954a0
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-11-26 23:52:41 -0600

    Refactor sensor packets into their own module
    This also moves the functions that deal with those values into the same module.

--- irobot.js
+++ irobot.js
@@ -7,100 +7,12 @@ var serialport = require('serialport');
 
 var commands = require('./commands');
 var demos = require('./demos');
-var oiEnums = require('./oi-enums');
+var sensors = require('./sensors');
 
 // convert a Hertz value into a MIDI number
 // from: http://en.wikipedia.org/wiki/MIDI_Tuning_Standard#Frequency_values
 var hzToMIDI = function (hz) {
   return Math.round(69 + 12 * (Math.log(hz / 440) / Math.log(2)));
-};
-
-// retrieve the sensor packet info that has the given id, or null none was
-// found. includes the lowercase name of the packet type on a 'name' property,
-// inluding the other data on its enum.
-var getSensorIdInfo = _.memoize(function (id) {
-  var result = null;
-
-  // find the sensor packet info that has the given id
-  _.each(oiEnums.SENSORS, function (info, name) {
-    if (info.id === id) {
-      // make a copy of the sensor info so we can modify it
-      result = _.extend({}, info);
-
-      // store the name on the info for easy access
-      result.name = name.toLowerCase();
-
-      // stop iteration since we found the result
-      return false;
-    }
-  });
-
-  return result;
-});
-
-// calculate the checksum of some packet data and return true if its valid
-var isValidPacket = function (packet) {
-  // format looks like:
-  //   [header][bytes-count][id-1][data-1...][id-2][data-2...][checksum]
-  //
-  // checksum is the sum of all bytes in a packet, header to checksum inclusive,
-  // that when bitwise-ANDed with 0xFF should produce 0. this is in contrast
-  // with what the docs claim, but this implementation is actually the correct
-  // one.
-
-  // if the packet header doesn't match, it's invalid
-  var packetHeader = packet[0];
-  if (packetHeader !== oiEnums.PACKET_HEADER) {
-    return false;
-  }
-
-  // if the length doesn't match, it's invalid. the "+ 3" accounts for the
-  // length byte, header, and checksum.
-  var packetLength = packet[1];
-  if (packetLength + 3 !== packet.length) {
-    return false;
-  }
-
-  // sum all the bytes in the packet
-  var checksum = 0;
-  for (var i = packet.length - 1; i >= 0; i--) {
-    checksum += packet[i];
-  }
-
-  // if the checksum is non-zero, it's invalid. otherwise it passed validation!
-  // jshint bitwise:false
-  return !(checksum & 0xFF);
-};
-
-// parse a complete data packet array and return the parsed results
-var parseSensorData = function (data) {
-  // read all the bytes as sensor data packets
-  var sensorData = {};
-  while (data.length > 0) {
-    // get the next packet it from the buffer
-    var value = data.shift();
-
-    // get the sensor packet for the value
-    var packetInfo = getSensorIdInfo(value);
-
-    // error if we can't parse this packet
-    if (!packetInfo) {
-      throw new Error('unrecognized packet id: ' + value);
-    }
-
-    // splice out the data bytes for this packet type from the remaining bytes
-    var bytes = data.splice(0, packetInfo.bytes);
-
-    // parse the bytes according to the sensor info's method and store the
-    // result under the name of the sensor packet. if there's no method, the
-    // data is effectively thrown away. the parse method needs a buffer so it
-    // can handle the bytes directly.
-    if (_.isFunction(packetInfo.parse)) {
-      sensorData[packetInfo.name] = packetInfo.parse(new Buffer(bytes));
-    }
-  }
-
-  return sensorData;
 };
 
 var Robot = function (options) {
@@ -137,9 +49,6 @@ var Robot = function (options) {
 
 util.inherits(Robot, events.EventEmitter);
 
-// inherit enums from the other module, to simplify access to them
-Robot.SENSORS = oiEnums.SENSORS;
-
 // collect serial data in an internal buffer until we receive an entire packet,
 // and then emit a 'packet' event so that packet can be specifically parsed.
 Robot.prototype._parseSerialData = function (emitter, data) {
@@ -148,14 +57,14 @@ Robot.prototype._parseSerialData = function (emitter, data) {
 
   // attempt to find a valid packet in our stored bytes
   for (var i = this._buffer.length; i >= 0; i--) {
-    if (this._buffer[i] === oiEnums.PACKET_HEADER) {
+    if (this._buffer[i] === sensors.PACKET_HEADER) {
       // the packet length byte value and the packet end index (exclusive)
       var packetLength = this._buffer[i + 1];
       var endIndex = i + packetLength + 3;
 
       // set our indexes if we got a valid packet
       var packet = this._buffer.slice(i, endIndex);
-      if (isValidPacket(packet)) {
+      if (sensors.isValidSensorPacket(packet)) {
         // discard all bytes up to the packet's last byte inclusive
         this._buffer.splice(0, endIndex);
 
@@ -163,7 +72,7 @@ Robot.prototype._parseSerialData = function (emitter, data) {
         packet = packet.slice(2, -1);
 
         // parse the sensor data and emit an event with it
-        this.emit('sensordata', parseSensorData(packet));
+        this.emit('sensordata', sensors.parseSensorData(packet));
 
         break;
       }
@@ -187,7 +96,7 @@ Robot.prototype._init = function () {
   // start streaming all sensor data. we manually collect the packet ids we need
   // since streaming with the special bytes (id < 7) returns funky responses
   // that require lots of special cases to parse.
-  var packets = _.pluck(oiEnums.SENSORS, 'id');
+  var packets = _.pluck(sensors.ALL_SENSOR_PACKETS, 'id');
   this._sendCommand(commands.Stream, packets.length, packets);
 
   // emit an event to alert that we're now ready to receive commands!
---
+++ sensors.js
@@ -0,0 +1,369 @@
+var _ = require('lodash');
+var extend = require('node.extend');
+
+// turn a buffer into an int intelligently depending on its length
+var bufferToInt = function (buffer, signed) {
+  // builds something like 'readUInt8' or 'readInt32'
+  var method = [
+    'read',
+    signed ? '' : 'U',
+    'Int',
+    buffer.length * 8,
+    buffer.length > 1 ? 'BE' : ''
+  ].join('');
+
+  return buffer[method](0);
+};
+
+// return a value as a percentage scaled to the given ranges
+var scaleValue = function (rawValue, options) {
+  var defaults = {
+    min_raw: 0,
+    min_actual: 0
+  };
+  options = extend(defaults, options);
+
+  var rangeRaw = options.max_raw - options.min_raw;
+  var rangeActual = options.max_actual - options.min_actual;
+  return (1.0 * rawValue / rangeRaw) * rangeActual;
+};
+
+// parse the first byte as a boolean and return it
+var parseBool = function (bytes) { return !!(bytes[0]); }
+
+// parse the given bytes into an unsigned integer
+var parseUnsigned = function (bytes) { return bufferToInt(bytes); }
+
+// parse the given bytes into a signed integer
+var parseSigned = function (bytes) { return bufferToInt(bytes, true); }
+
+// return the bits of a byte as a boolean array
+var parseBits = function (b) {
+  // jshint bitwise:false
+  return [
+    !!((b & 0x80) >> 7),
+    !!((b & 0x40) >> 6),
+    !!((b & 0x20) >> 5),
+    !!((b & 0x10) >> 4),
+    !!((b & 0x8) >> 3),
+    !!((b & 0x4) >> 2),
+    !!((b & 0x2) >> 1),
+    !!(b & 0x1)
+  ];
+};
+
+var buildParseInt = function (valueKey, signed) {
+  return function (bytes) {
+    var result = {};
+    result[valueKey] = bufferToInt(bytes, signed);
+    return result;
+  };
+};
+
+var buildParseScaled = function (actualValueKey, options) {
+  return function (bytes) {
+    var result = {};
+
+    result['min_' + actualValueKey] = options.min_actual || 0;
+    result['max_' + actualValueKey] = options.max_actual;
+    result[actualValueKey] = scaleValue(bufferToInt(bytes), options);
+
+    return result;
+  };
+};
+
+var buildParseMagnitude = function (options) {
+  return function (bytes) {
+    var result = {
+      min: options.min,
+      max: options.max,
+      range: options.max - options.min,
+      raw: bufferToInt(bytes)
+    };
+
+    result.magnitude = 1.0 * result.raw / result.range;
+
+    return result;
+  };
+};
+
+var Packet = function (name, id, bytes, parser) {
+  this.name = name;
+  this.id = id;
+  this.bytes = bytes;
+
+  // the parser is null by default, otherwise the specified function
+  this.parser = _.isFunction(parser) ? parser : function () {};
+};
+
+// proxy to the start function given on creation
+Packet.prototype.parse = function () {
+  return this.parser.apply(this, arguments);
+};
+
+//
+// EXPORTS
+//
+
+// the first byte in a packet always has this value
+module.exports.PACKET_HEADER = 19;
+
+// retrieve the sensor packet object with the given id, or null none was found
+module.exports.getById = function (id) {
+  return module.exports.SENSOR_PACKETS_BY_ID[id] || null;
+};
+
+// determine whether some sensor packet data is valid, and return true if so
+module.exports.isValidSensorPacket = function (packet) {
+  // format looks like:
+  //   [packet-header][bytes-count][id-1][data-1...][id-2][data-2...][checksum]
+  //
+  // checksum is the sum of all bytes in a packet, header to checksum inclusive,
+  // that when bitwise-ANDed with 0xFF should produce 0. this is in contrast
+  // with what the docs claim, but this implementation is actually the correct
+  // one.
+
+  // if the packet header doesn't match, it's invalid
+  var packetHeader = packet[0];
+  if (packetHeader !== module.exports.PACKET_HEADER) {
+    return false;
+  }
+
+  // if the length doesn't match, it's invalid. the "+ 3" accounts for the
+  // length byte, header, and checksum.
+  var packetLength = packet[1];
+  if (packetLength + 3 !== packet.length) {
+    return false;
+  }
+
+  // sum all the bytes in the packet
+  var checksum = 0;
+  for (var i = packet.length - 1; i >= 0; i--) {
+    checksum += packet[i];
+  }
+
+  // if the checksum is non-zero, it's invalid. otherwise it passed validation!
+  // jshint bitwise:false
+  return !(checksum & 0xFF);
+};
+
+// parse a complete data packet byte array and return the parsed results
+module.exports.parseSensorData = function (data) {
+  // read all the bytes as sensor data packets
+  var sensorData = {};
+
+  // we use a 'for' loop to ensure that it must eventually terminate
+  var i, length, packetId;
+  for (i = 0, length = data.length; (packetId = data[i]), i < length; i++) {
+    // get the sensor packet for the value
+    var packetInfo = module.exports.getById(packetId);
+
+    // error if we can't parse this packet
+    if (!packetInfo) {
+      throw new Error('unrecognized packet id:' + packetId);
+    }
+
+    // parse the packet's data bytes and store the result under the packet's
+    // name. if the parse function returns undefined, the data is thrown away.
+    // the parse method needs a buffer so it can handle the bytes directly.
+    var dataIndex = i + 1;
+    var bytes = data.slice(dataIndex, dataIndex + packetInfo.bytes);
+    var parsedData = packetInfo.parse(new Buffer(bytes));
+
+    if (!_.isUndefined(parsedData)) {
+      sensorData[packetInfo.name] = parsedData;
+    }
+
+    // skip over the bytes we just consumed
+    i += packetInfo.bytes;
+  }
+
+  return sensorData;
+};
+
+// create and export all the various sensor packet types
+_.extend(module.exports, {
+  BumpAndWheelDrop: new Packet('bump_and_wheel_drop', 7, 1, function (bytes) {
+    var bits = parseBits(bytes[0]);
+
+    var wheelDropCaster = bits[4];
+    var wheelDropLeft = bits[3];
+    var wheelDropRight = bits[2];
+    var bumpLeft = bits[1];
+    var bumpRight = bits[0];
+
+    return {
+      bump: {
+        left: bumpLeft,
+        right: bumpRight
+      },
+
+      wheel_drop: {
+        caster: wheelDropCaster,
+        left: wheelDropLeft,
+        right: wheelDropRight
+      }
+    };
+  }),
+
+  Wall: new Packet('wall', 8, 1, parseBool),
+  CliffLeft: new Packet('cliff_left', 9, 1, parseBool),
+  CliffFrontLeft: new Packet('cliff_front_left', 10, 1, parseBool),
+  CliffFrontRight: new Packet('cliff_front_right', 11, 1, parseBool),
+  CliffRight: new Packet('cliff_right', 12, 1, parseBool),
+  VirtualWall: new Packet('virtual_wall', 13, 1, parseBool),
+
+  Overcurrents: new Packet('low_side_driver_and_wheel_overcurrents', 14, 1, function (bytes) {
+    var bits = parseBits(bytes[0]);
+
+    var leftWheel = bits[4];
+    var rightWheel = bits[3];
+    var ld2 = bits[2];
+    var ld0 = bits[1];
+    var ld1 = bits[0];
+
+    return {
+      wheel: {
+        left: leftWheel,
+        right: rightWheel
+      },
+
+      // an array since it's just a map of LD index to value anyway
+      low_side_drivers: [
+        ld0,
+        ld1,
+        ld2
+      ]
+    };
+  }),
+
+  InfraredByte: new Packet('infrared_byte', 17, 1, function (bytes) {
+    var b = bytes[0];
+
+    return {
+      // b === 255 means "not receiving"
+      receiving: b !== 255,
+      value: b
+    };
+  }),
+
+  Buttons: new Packet('buttons', 18, 1, function (bytes) {
+    var bits = parseBits(bytes[0]);
+    return {
+      advance: bits[2],
+      play: bits[0]
+    };
+  }),
+
+  Distance: new Packet('distance', 19, 2, buildParseInt('millimeters', true)),
+  Angle: new Packet('angle', 20, 2, buildParseInt('degrees_ccw', true)),
+
+  ChargingState: new Packet('charging_state', 21, 1, function (bytes) {
+    var stateId = bytes[0];
+    return {
+      not_charging: stateId === 0,
+      reconditioning_charging: stateId === 1,
+      full_charging: stateId === 2,
+      trickle_charging: stateId === 3,
+      waiting: stateId === 4,
+      charging_fault_condition: stateId === 5
+    };
+  }),
+
+  Voltage: new Packet('voltage', 22, 2, buildParseInt('millivolts')),
+  Current: new Packet('current', 23, 2, buildParseInt('milliamp_hours')),
+
+  BatteryTemperature: new Packet('battery_temperature', 24, 1, buildParseInt('celcius', true)),
+  BatteryCharge: new Packet('battery_charge', 25, 2, buildParseInt('milliamp_hours')),
+  BatteryCapacity: new Packet('battery_capacity', 26, 2, buildParseInt('milliamp_hours')),
+
+  WallSignal: new Packet('wall_signal', 27, 2, buildParseMagnitude({
+    min: 0,
+    max: 4095
+  })),
+
+  CliffLeftSignal: new Packet('cliff_left_signal', 28, 2, buildParseMagnitude({
+    min: 0,
+    max: 4095
+  })),
+
+  CliffFrontLeftSignal: new Packet('cliff_front_left_signal', 29, 2, buildParseMagnitude({
+    min: 0,
+    max: 4095
+  })),
+
+  CliffFrontRightSignal: new Packet('cliff_front_right_signal', 30, 2, buildParseMagnitude({
+    min: 0,
+    max: 4095
+  })),
+
+  CliffRightSignal: new Packet('cliff_right_signal', 31, 2, buildParseMagnitude({
+    min: 0,
+    max: 4095
+  })),
+
+  CargoBayDigitalInputs: new Packet('cargo_bay_digital_inputs', 32, 1, function (bytes) {
+    var bits = parseBits(bytes[0]);
+
+    var deviceDetectBaudrateChange = bits[4];
+    var digitalInput3 = bits[3];
+    var digitalInput2 = bits[2];
+    var digitalInput1 = bits[1];
+    var digitalInput0 = bits[0];
+
+    return {
+      device_detect_baudrate_change: deviceDetectBaudrateChange,
+
+      digital_inputs: [
+        digitalInput0,
+        digitalInput1,
+        digitalInput2,
+        digitalInput3
+      ]
+    };
+  }),
+
+  CargoBayAnalogSignal: new Packet('cargo_bay_analog_signal', 33, 2, buildParseScaled('volts', {
+    max_raw: 1023,
+    max_actual: 5.0
+  })),
+
+  ChargingSourcesAvailable: new Packet('charging_sources_available', 34, 1, function (bytes) {
+    var bits = parseBits(bytes[0]);
+    return {
+      home_base: bits[1],
+      internal_charger: bits[0]
+    };
+  }),
+
+  OIMode: new Packet('oi_mode', 35, 1, function (bytes) {
+    var modeId = bytes[0];
+    return {
+      off: modeId === 0,
+      passive: modeId === 1,
+      safe: modeId === 2,
+      full: modeId === 3
+    };
+  }),
+
+  SongNumber: new Packet('song_number', 36, 1, parseUnsigned),
+  SongPlaying: new Packet('song_playing', 37, 1, parseBool),
+  NumberStreamPackets: new Packet('number_stream_packets', 38, 1, parseUnsigned),
+  RequestedVelocity: new Packet('requested_velocity', 39, 2, parseSigned),
+  RequestedRadius: new Packet('requested_radius', 40, 2, parseSigned),
+  RequestedRightVelocity: new Packet('requested_right_velocity', 41, 2, parseSigned),
+  RequestedLeftVelocity: new Packet('requested_left_velocity', 42, 2, parseSigned)
+});
+
+// an array of all sensor packets sorted by their id
+module.exports.ALL_SENSOR_PACKETS = _(module.exports).pick(function (p) {
+  return p instanceof Packet;
+}).values().sortBy('id').value();
+
+// create a map of packet ids to packets
+module.exports.SENSOR_PACKETS_BY_ID = {}
+_.each(module.exports, function (p) {
+  if (p instanceof Packet) {
+    module.exports.SENSOR_PACKETS_BY_ID[p.id] = p;
+  }
+});
