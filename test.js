7e376d3a9622c2653d16ef814cc6c818c8edb79e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-11-30 22:26:04 -0600

    Add overcurrent event emitting

--- test.js
+++ test.js
@@ -15,6 +15,7 @@ robot.on('button', function (e) { console.log('BUTTON', e); });
 robot.on('cliff', function (e) { console.log('CLIFF', e); });
 robot.on('ir', function (e) { console.log('IR', e); });
 robot.on('mode', function (e) { console.log('MODE', e); });
+robot.on('overcurrent', function (e) { console.log('OVERCURRENT', e); });
 robot.on('virtualwall', function (e) { console.log('VIRTUALWALL', e); });
 robot.on('wall', function (e) { console.log('WALL', e); });
 robot.on('wheeldrop', function (e) { console.log('WHEELDROP', e); });
